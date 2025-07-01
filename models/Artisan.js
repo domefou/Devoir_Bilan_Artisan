ae9ba815f89864345b7e5637184f76cc5b4ea3ca
Lecoultrejean <lecoultre.dev85@gmail.com>
2025-07-01 15:59:10 +0200

    correction sequelize

--- models/Artisan.js
+++ models/Artisan.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/sequelize'); // Assure-toi d'avoir une connexion Sequelize configurée
+const Sequelize = require('../config/sequelize'); // Assure-toi d'avoir une connexion Sequelize configurée
 
-const Artisan = sequelize.define('Artisan', {
+const Artisan = Sequelize.define('Artisan', {
     id_artisan: {
         type: DataTypes.INTEGER,
         primaryKey: true,
